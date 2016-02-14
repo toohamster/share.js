baf3615a0aeffa29e9173e833ef5a700d46beef1
xuexb <fe.xiaowu@gmail.com>
2016-02-14 16:44:06 +0800

    调整结构

--- src/js/share.js
+++ src/js/share.js
@@ -1,71 +1,64 @@
 /**
  * Share.js
+ * @file share.js
+ * @author @overtrue, @xiaowu修改
  */
 
 (function ($) {
 
     /**
-     * 是否为移动设备
+     * 实例化方法
      *
-     * @type {Boolean}
-     */
-    var isMobileScreen = !!navigator.userAgent.toLowerCase().match(/applewebkit.*mobile.*/);
-
-    /**
-     * 是否运行在微信浏览器
+     * @param  {Object} options 配置参数
      *
-     * @type {Boolean}
+     * @return {Object}          this
      */
-    var runningInWeChat = !!navigator.userAgent.toLowerCase().match(/MicroMessenger/);
-
-    /**
-     * 模板配置
-     *
-     * @type {Object}
-     */
-    var templates = {
-        wechat: 'javascript:void(0);',
-        qzone: 'http://sns.qzone.qq.com/cgi-bin/qzshare/cgi_qzshare_onekey?url={url}&title={title}&desc={description}&summary={summary}&site={source}',
-        qq: 'http://connect.qq.com/widget/shareqq/index.html?url={url}&title={title}&source={source}&desc={description}',
-        tencent: 'http://share.v.t.qq.com/index.php?c=share&a=index&title={title}&url={url}&pic={image}',
-        weibo: 'http://service.weibo.com/share/share.php?url={url}&title={title}&pic={image}',
-        // wechat: 'javascript:;',
-        douban: 'http://shuo.douban.com/!service/share?href={url}&name={title}&text={description}&image={image}&starid=0&aid=0&style=11',
-        diandian: 'http://www.diandian.com/share?lo={url}&ti={title}&type=link',
-        linkedin: 'http://www.linkedin.com/shareArticle?mini=true&ro=true&title={title}&url={url}&summary={summary}&source={source}&armin=armin',
-        facebook: 'https://www.facebook.com/sharer/sharer.php?u={url}',
-        twitter: 'https://twitter.com/intent/tweet?text={title}&url={url}&via={site_url}',
-        google: 'https://plus.google.com/share?url={url}',
-    };
-
-    $.fn.share = function ($options) {
-        if (!$.isPlainObject($options)) {
-            $options = {};
+    $.fn.share = function (options) {
+        if (!$.isPlainObject(options)) {
+            options = {};
         }
 
-        $options = $.extend({}, $.fn.share.defaults, $options);
+        options = $.extend({}, $.fn.share.defaults, options);
 
         return this.each(function () {
-            var data = $.extend({}, $options, $(this).data());
-            var $container = $(this).addClass('share-component').addClass('social-share');
+            var data = $(this).data();
+            var $container;
 
+            // 如果已经加载过
+            if (data._share) {
+                return;
+            }
+
+            // 打上标识
+            data._share = true;
+
+            // 合并配置，拿当前dom上的data-*来覆盖
+            data = $.extend({}, options, data);
+
+            // 添加类
+            $container = $(this).addClass('share-component social-share');
+
+            // 循环当前的站点
             $.each(getSites(data), function (index, name) {
-                var url = makeUrl(name, data);
                 var $elem;
 
+                // 如果已经初始化过dom，则查找这个dom
                 if (data.initialized) {
                     $elem = $container.find('.icon-' + name);
+
+                    // 如果dom不存在，则忽略
+                    if (!$elem.length) {
+                        return;
+                    }
                 }
                 else {
                     $elem = $('<a class="social-share-icon icon-' + name + '" target="_blank"></a>');
                 }
 
-                if (!$elem.length) {
-                    return;
-                }
+                // 设置链接
+                $elem.attr('href', makeUrl(name, data));
 
-                $elem.attr('href', url);
-
+                // 如果没有初始化过dom，则追加
                 if (!data.initialized) {
                     $container.append($elem);
                 }
@@ -91,52 +84,40 @@
     };
 
     /**
-     * Get available site lists.
+     * 模板配置
      *
-     * @param {Array} data
-     *
-     * @return {Array}
+     * @type {Object}
      */
-    function getSites(data) {
-        var sites = isMobileScreen ? data.mobileSites || data.sites : data.sites;
-
-        if (typeof sites === 'string') {
-            sites = sites.split(',');
-        }
-
-        sites = sites.filter(function (name) {
-            if (!name || !templates[name]) {
-                return false;
-            }
-
-            if (runningInWeChat && name === 'wechat') {
-                return false;
-            }
-        });
-
-        if (!sites.length) {
-            sites = $.fn.share.defaults.sites;
-        }
-
-        return sites;
-    }
+    $.fn.share.templates = {
+        wechat: 'javascript:void(0);',
+        qzone: 'http://sns.qzone.qq.com/cgi-bin/qzshare/cgi_qzshare_onekey?url={url}&title={title}&desc={description}&summary={summary}&site={source}',
+        qq: 'http://connect.qq.com/widget/shareqq/index.html?url={url}&title={title}&source={source}&desc={description}',
+        tencent: 'http://share.v.t.qq.com/index.php?c=share&a=index&title={title}&url={url}&pic={image}',
+        weibo: 'http://service.weibo.com/share/share.php?url={url}&title={title}&pic={image}',
+        // wechat: 'javascript:;',
+        douban: 'http://shuo.douban.com/!service/share?href={url}&name={title}&text={description}&image={image}&starid=0&aid=0&style=11',
+        diandian: 'http://www.diandian.com/share?lo={url}&ti={title}&type=link',
+        linkedin: 'http://www.linkedin.com/shareArticle?mini=true&ro=true&title={title}&url={url}&summary={summary}&source={source}&armin=armin',
+        facebook: 'https://www.facebook.com/sharer/sharer.php?u={url}',
+        twitter: 'https://twitter.com/intent/tweet?text={title}&url={url}&via={site_url}',
+        google: 'https://plus.google.com/share?url={url}'
+    };
 
     /**
-     * Build the url of icon.
+     * 是否手机浏览器
      *
-     * @param {String} name
-     * @param {Object} data
-     *
-     * @return {String}
+     * @type {Boolean}
      */
-    function makeUrl(name, data) {
-        data['summary'] = data['description'];
+    $.fn.share.isMobile = !!navigator.userAgent.toLowerCase().match(/applewebkit.*mobile.*/);
 
-        return templates[name].replace(/\{([^\}]+?)\}/g, function ($0, $1) {
-            return data[$1] !== undefined ? encodeURIComponent(data[$1]) : '';
-        });
-    }
+    /**
+     * 是否运行在微信浏览器
+     *
+     * @type {Boolean}
+     */
+    $.fn.share.isWeChat = !!navigator.userAgent.toLowerCase().match(/MicroMessenger/);
 
+    var $head = $(document.head);
     /**
      * 默认配置
      *
@@ -145,13 +126,13 @@
     $.fn.share.defaults = {
         url: window.location.href,
         site_url: window.location.origin,
-        source: $(document.head).find('[name="site"]').attr('content') || $(document.head).find('[name="Site"]').attr('content') || document.title,
-        title: $(document.head).find('[name="title"]').attr('content') || $(document.head).find('[name="Title"]').attr('content') || document.title,
-        description: $(document.head).find('[name="description"]').attr('content') || $(document.head).find('[name="Description"]').attr('content'),
-        image: $(document).find('img:first').attr('src'),
+        source: $head.find('[name="site"]').attr('content') || $head.find('[name="Site"]').attr('content') || document.title,
+        title: $head.find('[name="title"]').attr('content') || $head.find('[name="Title"]').attr('content') || document.title,
+        description: $head.find('[name="description"]').attr('content') || $head.find('[name="Description"]').attr('content'),
+        image: '',
         wechatQrcodeTitle: '微信扫一扫：分享',
         wechatQrcodeHelper: '<p>微信里点“发现”，扫一下</p><p>二维码便可将本文分享至朋友圈。</p>',
-        mobileSites: [],
+        mobileSites: null,
         sites: [
             'weibo',
             'qq',
@@ -167,8 +148,66 @@
         ]
     };
 
+    /**
+     * 获取站点，会做数据容错
+     *
+     * @param {Object} data 配置数据
+     *
+     * @return {Array} 可用的站点列表
+     */
+    function getSites(data) {
+        var sites = $.fn.share.isMobile ? data.mobileSites || data.sites : data.sites;
+
+        if (typeof sites === 'string') {
+            sites = sites.split(',');
+        }
+
+        // 筛选站点
+        sites = sites.filter(function (name) {
+            name = $.trim(name);
+
+            // 如果为空或者站点没有配置模板则忽略
+            if (!name || !$.fn.share.templates[name]) {
+                return false;
+            }
+
+            // 如果当前在微信浏览器里运行并且站点名是微信则忽略
+            if ($.fn.share.isWeChat && name === 'wechat') {
+                return false;
+            }
+
+            return true;
+        });
+
+        // 如果筛选站点后为空，则使用默认值
+        if (!sites.length) {
+            sites = $.fn.share.defaults.sites;
+        }
+
+        return sites;
+    }
+
+    /**
+     * 生成分享的跳转链接
+     *
+     * @param {string} name 站点名
+     * @param {Object} data 配置数据对象
+     *
+     * @return {string} 最终链接
+     */
+    function makeUrl(name, data) {
+        data.summary = data.description;
+
+        return $.fn.share.templates[name].replace(/\{([^\}]+?)\}/g, function ($0, $1) {
+            return data[$1] !== undefined ? encodeURIComponent(data[$1]) : '';
+        });
+    }
+
     // Domready after initialization
     $(function () {
-        $('.share-component,.social-share').share();
+        $('.share-component, .social-share').share();
+
+        // domready之后再获取图片链接
+        $.fn.share.defaults.image = $('body').find('img').eq(0).attr('src');
     });
 })(jQuery);
